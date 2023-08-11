92bbeadc3d444219460e86530c1fe2bb9d34c3ea
Melissa Rodriguez <melissa.rodriguez.murguia@gmail.com>
2023-08-11 14:33:15 -0600

    Added button and logic for managing loader

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,6 +8,8 @@ const Login = ( { setUser } ) => {
     const handleOnSubmit = async (e) => {
         e.preventDefault();
         try {
+            document.getElementById('login-button').hidden = true;
+            document.getElementById('loading-login-button').hidden = false;
             let response = await fetch(
             `http://localhost:5000/user/?username=${username}&password=${password}`, {
                 method: "get",
@@ -15,19 +17,25 @@ const Login = ( { setUser } ) => {
                     'Content-Type': 'application/json'
                 }
             })
-            
+
             let result = await response.json();
             if(result.statusCode === 200) {
                 setUser(result.data[0]);
             }
             else {
                 document.getElementById('incorrect-credentials').hidden = false;
+                document.getElementById('loading-login-button').hidden = true;
+                document.getElementById('login-button').hidden = false;
             }
             setPass("");
             setName("");  
         }
         catch(err) {
             document.getElementById('access-err').hidden = false;
+            document.getElementById('loading-login-button').hidden = true;
+            document.getElementById('login-button').hidden = false;
+            setPass("");
+            setName("");
             throw err;
         }
     }
@@ -36,8 +44,8 @@ const Login = ( { setUser } ) => {
     <>
         <h1>Zoo Authentication System</h1>
           
-        <p class="credentials-error-msg" id="incorrect-credentials" hidden="true">Incorrect username or password.</p>
-        <p class="credentials-error-msg" id="access-err" hidden="true">Cannot log you in at this time. Please try again later.</p>
+        <p class="credentials-error-msg" id="incorrect-credentials" hidden>Incorrect username or password.</p>
+        <p class="credentials-error-msg" id="access-err" hidden>Cannot log you in at this time. Please try again later.</p>
 
         <form action="">
             <label>Username:</label>
@@ -48,7 +56,8 @@ const Login = ( { setUser } ) => {
             <input type="password" placeholder="Enter password" 
             value={password} onChange={(e) => setPass(e.target.value)} />
               
-            <button type="submit" onClick={handleOnSubmit}>Log In</button>
+            <button type="submit" id="login-button" onClick={handleOnSubmit}>Log In</button>
+            <button type="submit" id="loading-login-button" hidden>Logging In</button>
         </form>  
     </>
   );
